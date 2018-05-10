0d1c3c8ee446c7ae2c2f2bb7a55e031f02389001
george <georgesims21@hotmail.com>
2018-05-10 15:38:23 +0200

    Updated descriptions

--- arith_operators.py
+++ arith_operators.py
@@ -1,8 +1,10 @@
-#Task
-#Read two integers from STDIN and print three lines where:
-#1.The first line contains the sum of the two numbers.
-#2.The second line contains the difference of the two numbers (first - second).
-#3.The third line contains the product of the two numbers.
+#------------------------------------------------------------------------------#
+# Read two integers from STDIN and print three lines where:
+# 1.The first line contains the sum of the two numbers.
+# 2.The second line contains the difference of the two numbers (first - second).
+# 3.The third line contains the product of the two numbers.
+#------------------------------------------------------------------------------#
+
 integerA = input()
 integerB = input()
 
